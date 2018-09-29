aaad99738834af6aed9b18bbc177713d9d73a3a8
peaw1998 <m.peaw5500@gmail.com>
2018-09-29 18:52:59 +0700

    Check Point 3

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View ,ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import Forecast from './Forecast';
 
 export default class Weather extends React.Component {
@@ -15,8 +15,10 @@ export default class Weather extends React.Component {
         return (
             <View style={styles.container}>
                 <ImageBackground source={require('./bg.jpeg')} style={styles.backdrop}>
-                    <Text>Zip code is {this.props.zipCode}.</Text>
-                    <Forecast {...this.state.forecast} />
+                    <View style={styles.flexDir}>
+                        <Text style={styles.text1}>Zip code is {this.props.zipCode}.</Text>
+                        <Forecast {...this.state.forecast} />
+                    </View>
                 </ImageBackground>
             </View>
         );
@@ -25,4 +27,13 @@ export default class Weather extends React.Component {
 const styles = StyleSheet.create({
     container: { paddingTop: 25 },
     backdrop: { width: '100%', height: '100%' },
+    flexDir: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 360,
+        backgroundColor: 'black',
+        opacity: 0.5,
+    },
+    text1: { textAlign: 'center', fontSize: 20, color: 'white', opacity: 0.5, },
 });
