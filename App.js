833f4f3947a44b881cb729110663f3e345249bfa
peaw1998 <m.peaw5500@gmail.com>
2018-09-29 19:50:28 +0700

    Route

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import Weather from './components/Weather';
+import ZipCodeScreen from './components/ZipCodeScreen';
+import WeatherScreen from './components/WeatherScreen';
+import { createStackNavigator } from 'react-navigation';
+const RootStack = createStackNavigator({
+  Weather: WeatherScreen,
+  ZipCode: ZipCodeScreen
+}, {
+    initialRouteName: 'Weather',
+    //initialRouteName: 'ZipCode',
+  })
 export default class App extends React.Component {
   render() {
     return (
-      <Weather zipCode="90110"/>
+      <RootStack />
     );
   }
 }
